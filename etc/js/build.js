80eb4fef4bef214cfc4198e04e9a025c1749d690
Mike Gioia <mikegioia@gmail.com>
2014-08-11 14:47:08 -0400

    Finalized build js. Found a weird issue with the browserify options in the 4.2.x version we're using. It seems like it's resolved in 5.x.

--- etc/js/build.js
+++ etc/js/build.js
@@ -1,151 +1,125 @@
-/*
- *  build.js
- *  Packages node modules, other vendor js, and custom js
- *  using Browserify.
+#!/usr/bin/env node
+/**
+ * build.js
+ * Packages node modules, other vendor js, and custom js
+ * using Browserify.
  *
- *  To run:
- *  $> node etc/js/build.js [--debug] [--minify] [module|all]
+ * To run:
+ * $> node etc/js/build.js [--debug] [--minify] [module|all]
  */
-
-
 'use strict';
 
-// required before all
+// Required before all
 var fs = require( 'fs' )
   , _ = require( 'underscore' )
   , async = require( 'async' )
   , mkdirp = require( 'mkdirp' )
+  , colors = require( 'colors' )
   , childProcess = require( 'child_process' )
+  , program = require( 'commander' )
   , browserify = require( 'browserify' );
 
-var scriptPath = __dirname;
-
-// check if config files exist
-var config = JSON.parse(
-        fs.readFileSync( scriptPath + '/../config.json', 'utf8' ) )
+// Get base paths, config files
+var scriptPath = __dirname
+  , config = JSON.parse(
+        fs.readFileSync(
+            scriptPath + '/../config.json',
+            'utf8'
+        ))
   , defaults = JSON.parse(
-        fs.readFileSync( scriptPath + '/../defaults.json', 'utf8' ) )
-  , settings = _.extend( defaults, config );
-
-// pull out paths
-var basePath = scriptPath + '/' + settings.devPaths.basePath
+        fs.readFileSync(
+            scriptPath + '/../defaults.json',
+            'utf8'
+        ))
+  , settings = _.extend( defaults, config )
+  , basePath = scriptPath + '/' + settings.devPaths.basePath
   , modulePath = scriptPath + '/' + settings.devPaths.modulePath
   , vendorPath = scriptPath + '/' + settings.devPaths.vendorPath
-  , buildPath = scriptPath + '/' + settings.devPaths.buildPath;
+  , buildPath = scriptPath + '/' + settings.devPaths.buildPath
+  , flags = {
+        minify: false,
+        debug: false }
+  , modules = [ 'all' ];
 
+// Set up commander
+program
+    .version( '1.0.0' )
+    .usage( '[options] [modules|all]' )
+    .option( '-d, --debug', 'Build browserify with debug option' )
+    .option( '-m, --minify', 'Minify browserify bundles with uglifyify' )
+    .parse( process.argv );
 
-var flags = { 'debug' : false, 'minify' : false };
-
-// Determine which module to build. If script is run with and
-// argument, use that. Possible flags are minify or debug.
-//
-// TODO: Check if debug flag is properly being passed to browserify.
-
-console.log( '\n======================================================' );
-
-if ( process.argv.length > 2 ) {
-    var index;
-
-    process.argv.splice( 0,2 );
-
-    index = process.argv.indexOf( '--debug' );
-    if ( index > -1 ) {
-        process.argv.splice( index, 1 );
-        flags.debug = true;
+// Set up flags
+function initFlags ( callback ) {
+    if ( ! _.isUndefined( program.minify ) ) {
+        flags.minify = program.minify;
     }
-
-    index = process.argv.indexOf( '--minify' );
-    if ( process.argv.indexOf( '--minify' ) > -1 ) {
-        process.argv.splice( index, 1 );
-        flags.minify = true;
+    if ( ! _.isUndefined( program.debug ) ) {
+        flags.debug = program.debug;
     }
-
-    if ( process.argv.length === 0 ||
-            ( process.argv.indexOf( 'all' ) > -1 &&
-                process.argv.indexOf( 'base' ) <= -1 ) ) {
-        process.argv.unshift( 'base' );
-    }
-
-    console.log( 'Attempting to build modules: ' + process.argv.join( ', ' ) );
-    process.argv.forEach( function ( module ) {
-        builder( module );
-    } );
-}
-else {
-    console.log( 'Attempting to build base' );
-    builder( 'base' );
-
+    callback( null, "Initialized flags" );
 }
 
+// Set up the modules to run
+function initModules ( callback ) {
+    if ( program.args.length ) {
+        modules = program.args;
+    }
+    callback( null, "Initialized modules" );
+}
 
-function builder ( module ) {
-    console.log( flags );
-
-    // create asset folders
+// Build the specified modules with the flags
+function build ( callback ) {
+    // create public asset folders
     mkdirp( buildPath + '/js' );
     mkdirp( buildPath + '/css' );
 
-    if ( module === 'base' ) {
-        buildBase( );
+    // check for 'all' or 'base' keywords
+    if ( _.indexOf( modules, 'base' ) !== -1 ) {
+        buildBase();
+        modules = _.without( modules, 'base' );
     }
-    else if ( module === 'all' ) {
+    else if ( _.indexOf( modules, 'all' ) !== -1 ) {
+        buildBase();
+        modules = [];
         _.each( settings.modules, function ( value, key ) {
-            buildModule( key );
+            modules.push( key );
         });
     }
-    else {
+
+    // iterate through module lists and compile them
+    _.each( modules, function ( module ) {
         buildModule( module );
-    }
+    });
 
-
-        // settings.modules.forEach( function ( mod ) {
-        //     buildModule( mod );
-        // } );
+    callback( null, "Finished build" );
 }
 
-function buildBase ( ) {
-
-    //  browserifyModules(module, entries, outFile, settings)
+// Build the base module
+function buildBase () {
+    // build the bundle
     browserifyModules(
         'base',
-        [ scriptPath + '/../../junk.js' ],
+        null,
         buildPath + 'js/base.js',
-        settings.base.browserify
-    );
-
-    async.series( [
-        copyJsFiles,
+        settings.base.browserify );
+    // and copy the assets
+    async.series([
+        copyBaseJsFiles,
         lessCssFiles,
-        copyCssFiles
+        copyBaseCssFiles
     ], function ( err, results ) {
-        if ( err ) { console.log( err ); }
-        console.log( results.join( '\n' ) );
-    } );
-}
-
-function buildModule ( module ) {
-
-    // check that module path exists
-    if ( fs.existsSync( modulePath + module + '/index.js' ) ) {
-
-        browserifyModules(
-            module,
-            [ modulePath + module + '/index.js' ],
-            buildPath + 'js/' + module + '.js',
-            settings.modules[module].browserify
-        );
-
-        lessCssFiles( null, module );
-    }
-    else {
-        throw new Error(
-            'Module does not seem to exist. Looked for ' + modulePath +
-            module + '/index.js' );
-    }
+        if ( err ) {
+            console.log( ('ERROR: ' + err).blackBG.red.bold );
+        }
+        else {
+            console.log( results.join( '\n' ) );
+        }
+    });
 }
 
 // Copy any JS files
-function copyJsFiles ( callback ) {
+function copyBaseJsFiles ( callback ) {
     var jsPath = buildPath + 'js/';
 
     if ( config.base.js.value.length > 0 ) {
@@ -153,23 +127,22 @@ function copyJsFiles ( callback ) {
             function ( jsFile ) {
                 fs.createReadStream( vendorPath + jsFile.source )
                     .pipe( fs.createWriteStream( jsPath + jsFile.target ) );
-            } );
+            });
     }
 
-    // callback(new Error( 'This is a test' ));
-    callback( null,
-        'Vendor JS files copied to ' + jsPath.replace( __dirname, '' )
-    );
+    callback(
+        null,
+        'Vendor JS files copied to ' + jsPath.replace( __dirname, '' ) );
 }
 
 // Copy and concatenate any CSS files into base.css
-function copyCssFiles ( callback ) {
+function copyBaseCssFiles ( callback ) {
     if ( config.base.css.value.length > 0 ) {
         var cssPath = buildPath + 'css/base.css';
-        concat( {
+        concat({
             src: config.base.css.value,
             dest: cssPath
-        } );
+        });
     }
 
     callback( null, 'Vendor CSS files concatenated into base.css' );
@@ -177,46 +150,64 @@ function copyCssFiles ( callback ) {
 
 // Compile the LESS files and concatenate into base.css
 function lessCssFiles ( callback, module ) {
-    var inFile =
-            ( ( module === undefined ) ? basePath :
-                modulePath + module + '/' ) +
-                'styles/index.less'
-      , outFile = ( module === undefined ) ?
-            buildPath + 'css/base.css' :
-            buildPath + 'css/' + module + '.css'
+    var inFile = ( module === undefined )
+            ? basePath + '/styles/index.less'
+            : modulePath + module + '/' + 'styles/index.less'
+      , outFile = ( module === undefined )
+            ? buildPath + 'css/base.css'
+            : buildPath + 'css/' + module + '.css'
       , command = 'lessc ' + inFile + ' ' + outFile;
-        // Run the command
 
+    // Run the command
     var lesscProcess = childProcess.exec(
         command,
         function ( error, stdout, stderr ) {
-            if ( stdout ) { console.log( 'stdout: ' + stdout ); }
-            if ( stderr ) { console.log( 'stderr' + stderr ); }
-            if ( error !== null ) {
-                console.log( 'exec error: ' + error );
+            if ( stdout ) {
+                //console.log( 'stdout: ' + stdout );
             }
-        } );
+            if ( stderr ) {
+                console.log( ('ERROR: (exec) ' + stderr).blackBG.red.bold );
+            }
+            if ( error !== null ) {
+                console.log( ('ERROR: (exec) ' + error).blackBG.red.bold );
+            }
+        });
 
-    lesscProcess.on( 'close', function ( ) {
-        // console.log( 'Completed LESS and added to' + outFile );
-    } );
     if ( module === undefined ) {
-        callback( null,
-            'LESS CSS compiled into ' + outFile.replace( __dirname, '' )
-        );
+        callback( null, 'LESS CSS compiled into ' + outFile.replace( __dirname, '' ) );
     }
     else {
-        console.log( 'LESS CSS compiled into ' +
-            outFile.replace( __dirname, '' ) );
+        console.log( 'LESS CSS compiled into ' + outFile.replace( __dirname, '' ) );
     }
-
 }
 
+// Build a regular module
+function buildModule ( module ) {
+    // check that module path exists
+    if ( fs.existsSync( modulePath + module + '/index.js' ) ) {
+        // build the bundle
+        browserifyModules(
+            module,
+            [ modulePath + module + '/index.js' ],
+            buildPath + 'js/' + module + '.js',
+            settings.modules[ module ].browserify );
+        // less the CSS
+        lessCssFiles( null, module );
+    }
+    else {
+        console.log(
+            ('Module does not seem to exist. Looked for ' + modulePath +
+            module + '/index.js').blackBG.red.bold );
+        return false;
+    }
+}
+
+// Helper method for browserifying things
 function browserifyModules ( module, entries, outFile, rules ) {
-    var bundle = browserify( {
+    var bundle = browserify({
             debug: flags.debug
           , entries: entries
-        } );
+        });
 
     // Transforms from browserify config
     if ( rules.transforms.length > 0 ) {
@@ -226,32 +217,32 @@ function browserifyModules ( module, entries, outFile, rules ) {
             transformLib = require( transform );
             // bundle.transform it
             bundle.transform( transformLib );
-        } );
+        });
     }
 
     if ( flags.minify ) {
         bundle.transform( 'uglifyify' );
     }
 
-
-    // Excludes from browserify configs
-    rules.excludes.forEach( function ( exclusion ) {
-        bundle.exclude( exclusion );
-    } );
-
     // Require vendor libraries from browserify configs
     rules.requires.forEach( function ( dependency ) {
         bundle.require( dependency );
-    } );
+    });
+
+        // Excludes from browserify configs
+    rules.excludes.forEach( function ( exclusion ) {
+        bundle.exclude( exclusion );
+    });
 
     // Actually make bundle
     bundle
-        .bundle( function ( err, data ) {
-            if ( err ) { throw err; }
-            console.log( module + ' bundle created at' +
-                outFile.replace( __dirname, '' ) + '\n' );
+        .bundle({
+            debug: flags.debug
+        }, function ( err, data ) {
+            if ( err ) throw err;
+            console.log( module.yellow + ' bundle created at ' + outFile.replace( __dirname, '' ) );
             return data;
-        } )
+        })
         .pipe( fs.createWriteStream( outFile ) );
 }
 
@@ -261,8 +252,18 @@ function concat ( options ) {
       , fileEncoding = 'utf-8'
       , out = fileList.map( function ( filePath ) {
             return fs.readFileSync( vendorPath + filePath, fileEncoding );
-        } );
-
-    // console.log( fileList );
+        });
     fs.writeFileSync( outPath, out.join( '\n' ), fileEncoding );
 }
+
+// Run the program
+async.series([
+    initFlags,
+    initModules,
+    build
+],
+function ( err, results ) {
+    if ( err ) {
+        console.log( err );
+    }
+});
